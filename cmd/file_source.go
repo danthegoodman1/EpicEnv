e14568b1b8cc9a769add90ae0e032dc08cccf940
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2024-10-11 06:05:51 -0700

    fix extra format

--- cmd/file_source.go
+++ cmd/file_source.go
@@ -16,7 +16,7 @@ else
 fi
 temp_src=$(%s zzz_INTERNAL_gen -e %s)
 if [ $? -lt 1 ]; then
-	echo activated env default
+	echo activated env %s
     source $temp_src
     if [ -z "${EPICENV_DEV}" ]; then
         rm $temp_src
@@ -26,7 +26,7 @@ if [ $? -lt 1 ]; then
 fi
 `
 
-	err := os.WriteFile(path.Join(".epicenv", env, "activate"), []byte(fmt.Sprintf(activateContent, lo.Ternary(os.Getenv("EPICENV_DEV") != "", "go run .", "epicenv"), env)), 0777)
+	err := os.WriteFile(path.Join(".epicenv", env, "activate"), []byte(fmt.Sprintf(activateContent, lo.Ternary(os.Getenv("EPICENV_DEV") != "", "go run .", "epicenv"), env, env)), 0777)
 	if err != nil {
 		return fmt.Errorf("error in os.WriteFile: %w", err)
 	}
